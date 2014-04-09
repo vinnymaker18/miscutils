48ecbf86484ce31bc49c46b841e7d26e5acb5da4
Vinay Emani <vinayemani@gmail.com>
2014-04-09 14:03:16 +0530

    MyHTMLParser class being implemented

--- misc/hacker_news.py
+++ misc/hacker_news.py
@@ -8,9 +8,10 @@ class MyHTMLParser(HTMLParser.HTMLParser):
     """Parses the html text into a dict and returns it.
     """
     def __init__(self):
-        super(MyHTMLParser, self).__init__()
+        HTMLParser.HTMLParser.__init__(self)
         self.tag_to_data = dict()
         self.tag_stack = []
+        self.current_data = None
 
     def handle_starttag(self, tag, attrs):
         self.tag_stack.append(tag)
@@ -19,14 +20,27 @@ class MyHTMLParser(HTMLParser.HTMLParser):
         if not self.tag_stack or self.tag_stack[-1] != tag:
             raise ValueError("Invalid html data passed.")
 
+        tag = self.tag_stack.pop()
+        if self.current_data:
+            self.tag_to_data[tag] = self.current_data
+            self.current_data = None
+        
     def handle_data(self, data):
-        print "data received is ", data
+        self.current_data = data
+
+    def get_html_contents(self):
+        """Should be called after the html content is fed into this parser.
+        """
+        return self.tag_to_data 
 
 
 def main():
-    html_text = '<html></html>'
+    html_text = "<html><head><title>Simple text</title></head>\
+         <body>Data</body></html>"
     parser = MyHTMLParser()
     parser.feed(html_text)
+    html_contents = parser.get_html_contents()
+    print html_contents
 
 if __name__ == '__main__':
     main()
